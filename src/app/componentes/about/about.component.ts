82d1f4c73936d9fedd55ecaaeb83edd6b5abeef0
Hernán Danilo Escobar <hescobar@ext.softtek.com>
2024-04-19 00:14:01 -0300

    Se comenzon a trabajar en el componente about

--- src/app/componentes/about/about.component.ts
+++ src/app/componentes/about/about.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
